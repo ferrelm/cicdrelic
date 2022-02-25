8f413eb2c22d3952a48c06b2e397d3a4f9e2e9fd
ferrelm <31996387+ferrelm@users.noreply.github.com>
2022-02-25 18:16:26 +0000

    relic

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+
+# ricardo.coelho@akapeople.pt
+# ruck@chu5WRET3pu
+
+
+
+require('newrelic');
+
 const express = require('express')
 const app = express()
 const version = process.env.VERSION || 'local'
 
+const ERROR_PERCENTAGE = 10
+const SLOW_FACTOR = 1
 
 const getMessage = () => {
   const message = `Hello World! \n Version: ${version}`
@@ -11,8 +21,28 @@ const getMessage = () => {
     return message
 }
 
+const mySlowFunction = () => {
+  let result = 0
+  for (var i = Math.pow(SLOW_FACTOR, 7); i >= 0; i--) {
+    result += Math.atan(i) * Math.tan(i)
+  };
+}
+
+const shouldBeError = () => {
+  const randomNumber = Math.floor(Math.random() * 100)
+  return randomNumber < ERROR_PERCENTAGE
+}
+
 app.get('/', (req, res) => {
-  res.send(getMessage())
+  mySlowFunction()
+
+  if (shouldBeError()){
+    console.log("ERROR")
+    return res.status(500).send("Ooops!")    
+  }
+
+  console.log("OK")
+  res.status(200).send("Hello!")
 })
 
 module.exports = app;
